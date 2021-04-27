338e414643caa59b637d7c94a4a5b373068b1e28
Bianca <biancaribeiro2004@gmail.com>
2021-04-26 21:16:20 -0300

    .

---
+++ script.js
@@ -0,0 +1,113 @@
+function bateria() {
+    if(event.keyCode == 65){
+        new Audio("Sons1/bumbo.mp3").play()
+    }
+    else if(event.keyCode == 83){
+        new Audio("Sons1/caixa.mp3").play()
+    }
+    else if(event.keyCode == 68){
+        new Audio("Sons1/crash.mp3").play()
+    }
+    else if(event.keyCode == 70){
+        new Audio("Sons1/crash2.mp3").play()
+    }
+    else if(event.keyCode == 71){
+        new Audio("Sons1/hihat1.mp3").play()
+    }
+    else if(event.keyCode == 72){
+        new Audio("Sons1/ride.mp3").play()
+    }
+    else if(event.keyCode == 74){
+        new Audio("Sons1/surdo.mp3").play()
+    }
+    else if(event.keyCode == 75){
+        new Audio("Sons1/tom1.mp3").play()
+    }
+    else if(event.keyCode == 76){
+        new Audio("Sons1/tom2.mp3").play()
+    }
+}
+
+function piano() {
+    if(event.keyCode == 81){
+        new Audio("Sons2/a-3.mp3").play()
+    }
+    else if(event.keyCode == 87){
+        new Audio("Sons2/a-4.mp3").play()
+    }
+    else if(event.keyCode == 69){
+        new Audio("Sons2/a-5.mp3").play()
+    }
+    else if(event.keyCode == 82){
+        new Audio("Sons2/a3.mp3").play()
+    }
+    else if(event.keyCode == 84){
+        new Audio("Sons2/a4.mp3").play()
+    }
+    else if(event.keyCode == 89){
+        new Audio("Sons2/a5.mp3").play()
+    }
+    else if(event.keyCode == 85){
+        new Audio("Sons2/b3.mp3").play()
+    }
+    else if(event.keyCode == 73){
+        new Audio("Sons2/b4.mp3").play()
+    }
+    else if(event.keyCode == 79){
+        new Audio("Sons2/b5.mp3").play()
+    }
+    else if(event.keyCode == 80){
+        new Audio("Sons2/c-3.mp3").play()
+    }
+    else if(event.keyCode == 65){
+        new Audio("Sons2/c-4.mp3").play()
+    }
+    else if(event.keyCode == 83){
+        new Audio("Sons2/c-5.mp3").play()
+    }
+    else if(event.keyCode == 68){
+        new Audio("Sons2/c3.mp3").play()
+    }
+    else if(event.keyCode == 70){
+        new Audio("Sons2/c4.mp3").play()
+    }
+    else if(event.keyCode == 71){
+        new Audio("Sons2/c5.mp3").play()
+    }
+    else if(event.keyCode == 72){
+        new Audio("Sons2/c6.mp3").play()
+    }
+    else if(event.keyCode == 74){
+        new Audio("Sons2/d-3.mp3").play()
+    }
+    else if(event.keyCode == 75){
+        new Audio("Sons2/d-4.mp3").play()
+    }
+    else if(event.keyCode == 76){
+        new Audio("Sons2/d-5.mp3").play()
+    }
+    else if(event.keyCode == 90){
+        new Audio("Sons2/d3.mp3").play()
+    }
+    else if(event.keyCode == 88){
+        new Audio("Sons2/d4.mp3").play()
+    }
+    else if(event.keyCode == 67){
+        new Audio("Sons2/d5.mp3").play()
+    }
+    else if(event.keyCode == 86){
+        new Audio("Sons2/e3.mp3").play()
+    }
+    else if(event.keyCode == 66){
+        new Audio("Sons2/e4.mp3").play()
+    }
+    else if(event.keyCode == 78){
+        new Audio("Sons2/e5.mp3").play()
+    }
+    else if(event.keyCode == 77){
+        new Audio("Sons2/f-3.mp3").play()
+    }
+    else if(event.keyCode == 32){
+        new Audio("Sons2/f-4.mp3").play()
+    }
+}  
